3df456674acbbd7258af4d9cfc7afdaee63c8d43
Lee Sangyeop <cstria0106@gmail.com>
2024-05-08 11:28:45 +0900

    Update

--- node.h
+++ node.h
@@ -2,11 +2,9 @@
 #define NODE_H
 
 class Node {
-  // TODO: Node 클래스 정의 추가 작성
+  // TODO: Node 클래스 정의 수정할 것. 함수, 변수 추가 불가능
 
 public:
-  int value() { return value_; }
-
   Node(int value) {
     value_ = value;
     next_ = nullptr;
